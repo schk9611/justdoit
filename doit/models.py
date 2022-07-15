ff9a39f3ab4d4d28a66058e424d94a76f13878c1
gyuchan <schk9611@gmail.com>
2022-07-15 17:22:17 +0900

    Add: models.py에 User 클래스 생성

--- doit/models.py
+++ doit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Models):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    etc_info = models.CharField(max_length=500, null=True)
+
+    class Meta:
+        db_table = 'users'
